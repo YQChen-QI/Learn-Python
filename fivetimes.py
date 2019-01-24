82a7a4521f452ba403c0655325822b7d766c4c97
Yuquan <cyqustc@mail.ustc.edu.cn>
2019-01-24 09:44:19 +0800

    Update fivetimes.py

--- fivetimes.py
+++ fivetimes.py
@@ -1,3 +1,11 @@
+print('code using for loop:')
 print('My name is')
 for i in range(5):
     print('Jimmy Five Times ('+str(i)+')')
+
+print('code using while loop:')
+print('My name is')
+i = 0
+while i<5:
+    print('Jimmy Five Times ('+str(i)+')')
+    i = i + 1
